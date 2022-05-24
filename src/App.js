b4d721795e7f89bf9a268dac8802b0d688065c3c
alberto domingues <investimentofvcerto@gmail.com>
2022-05-24 15:51:54 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect} from 'react';
+import { ThemeContext, themes } from './Theme'
+import Card from './Card'
+
+function App ()  {
+  const [token, setToken] = useState()
+
+  useEffect(() => {
+    setTimeout(() => {
+      setToken('34q4m4k3ak43ak4kak4')
+    }, 4000)
+      
+  }, [setToken])
+    
+  return(
+    <ThemeContext.Provider value={themes.primary}>
+      <Card />
+    </ThemeContext.Provider>
+  )
+}
+
+export default APP
+
+
+
+//09:00
